6cd1e5142bc3bc8de5475205b0c0b7d26c663f8d
Omar Magdy <contact.parmag@gmail.com>
2021-05-25 12:30:20 +0200

    tests.py:add:sample test

---
+++ src/tests.py
@@ -0,0 +1,22 @@
+import unittest
+import json
+
+from app import app
+
+class CITestCase(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app
+        self.client = self.app.test_client
+        
+
+    def test_001_test(self):
+        response = self.client().get("/")
+        self.assertEqual(response.status_code,200)
+        data = json.loads(response.data)
+        self.assertEqual(data["success"],True)
+
+
+# Make the tests conveniently executable
+if __name__ == "__main__":
+    unittest.main()
