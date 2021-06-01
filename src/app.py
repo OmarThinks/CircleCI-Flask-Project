f1ea2f39bb1fc43ff75486896a48d3646dbe94d1
Omar Magdy <contact.parmag@gmail.com>
2021-06-01 09:18:21 +0200

    app.py:add:python to print env variable

--- src/app.py
+++ src/app.py
@@ -1,9 +1,11 @@
 from flask import Flask
+import os
 
 app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
+	print(os.environ['message'] ,flush=True)
     return {"success":True, "message":"Hello, World!"}
 
 
