2055eadde325c9cb2d73bac085c15ee96a53f998
Omar Magdy <contact.parmag@gmail.com>
2021-06-01 09:20:03 +0200

    app.py:fixed syntax error

--- src/app.py
+++ src/app.py
@@ -6,8 +6,8 @@ app = Flask(__name__)
 @app.route("/")
 def hello_world():
 	print(os.environ['message'] ,flush=True)
-    return {"success":True, "message":"Hello, World!"}
+	return {"success":True, "message":"Hello, World!"}
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+	app.run(debug=True)
