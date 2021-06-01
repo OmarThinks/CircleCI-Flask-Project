b66df78a11375b2fd43c3769ac218998e0523566
Omar Magdy <contact.parmag@gmail.com>
2021-06-01 09:24:02 +0200

    app.py:mod:env variable in response

--- src/app.py
+++ src/app.py
@@ -5,8 +5,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-	print(os.environ['message'] ,flush=True)
-	return {"success":True, "message":"Hello, World!"}
+	return {"success":True, "message":os.environ['message']}
 
 
 if __name__ == "__main__":
